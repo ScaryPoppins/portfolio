4d5d921d55fb8f94261ff4c853ae69cfb969cba1
Mark Nagel <mark_nagel@hotmail.com>
2019-07-14 13:50:26 -0500

    Updates

---
+++ precache-manifest.40fade0b0eb0dbfe00aec42ed79f8bc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf23a10a286b5f6ca9d549bd89479d36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8871b55ab807007b28cf",
+    "url": "/static/css/2.606264ab.chunk.css"
+  },
+  {
+    "revision": "2c73f6ad7bb1177eab78",
+    "url": "/static/css/main.25ecffb2.chunk.css"
+  },
+  {
+    "revision": "8871b55ab807007b28cf",
+    "url": "/static/js/2.a20a535f.chunk.js"
+  },
+  {
+    "revision": "2c73f6ad7bb1177eab78",
+    "url": "/static/js/main.f2be8f91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd56c506a83cc28bbce10cb94f444c07",
+    "url": "/static/media/Linkedin.bd56c506.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.937fd5a95e35ce6858d7561f288500f6.js"
+  "/precache-manifest.40fade0b0eb0dbfe00aec42ed79f8bc2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
